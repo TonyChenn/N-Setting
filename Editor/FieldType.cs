959ecb417e6f7b337f3c02dc75f142aba5410e9d
tonychenn <tonychenn@qq.com>
2022-03-10 19:32:14 +0800

    Revert "修改命名空间"

--- Editor/FieldType.cs
+++ Editor/FieldType.cs
@@ -1,25 +1,26 @@
-﻿namespace SettingKit.Editor
+﻿/// <summary>
+/// 字段类型
+/// </summary>
+public enum FieldType
 {
     /// <summary>
-    /// 字段类型
+    /// 按钮
     /// </summary>
-    public enum FieldType
-    {
-        /// <summary>
-        /// 文本框
-        /// </summary>
-        TextField,
-        /// <summary>
-        /// 选择文件夹
-        /// </summary>
-        Folder,
-        /// <summary>
-        /// 选择文件
-        /// </summary>
-        File,
-        /// <summary>
-        /// Toggle
-        /// </summary>
-        Toggle,
-    }
+    Button,
+    /// <summary>
+    /// 文本框
+    /// </summary>
+    TextField,
+    /// <summary>
+    /// 选择文件夹
+    /// </summary>
+    Folder,
+    /// <summary>
+    /// 选择文件
+    /// </summary>
+    File,
+    /// <summary>
+    /// Toggle
+    /// </summary>
+    Toggle,
 }
--- Editor/SettingAttribute.cs
+++ Editor/SettingAttribute.cs
@@ -1,30 +1,50 @@
 ﻿using System;
 
-namespace SettingKit.Editor
+
+/// <summary>
+/// 只能用于字段
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class SettingPropertyAttribute : Attribute
 {
-    /// <summary>
-    /// 只能用于字段
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class SettingAttribute:Attribute
+    private FieldType fieldType;
+    private string title;
+    private string text;
+    private Action action;
+
+    public SettingPropertyAttribute(FieldType fieldType, string title)
     {
-        private FieldType fieldType;
-        private string title;
-
-        public SettingAttribute(FieldType fieldType, string title)
-        {
-            this.fieldType = fieldType;
-            this.title = title;
-        }
-
-        public string Title
-        {
-            get { return title; }
-        }
-        
-        public FieldType FieldType
-        {
-            get { return fieldType; }
-        }
+        this.fieldType = fieldType;
+        this.title = title;
     }
+    public SettingPropertyAttribute(string title, string text, Action action)
+    {
+        this.fieldType = FieldType.Button;
+        this.title = title;
+        this.text = text;
+    }
+
+    public string Title { get { return title; } }
+    public FieldType FieldType { get { return fieldType; } }
+    public string Text { get { return text; } }
+    public Action Callback { get { return action; } }
+}
+
+/// <summary>
+/// 只能用于方法
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class SettingMethodAttribute : Attribute
+{
+    private string title;
+    private string text;
+
+    public SettingMethodAttribute(string title, string text)
+    {
+        this.title = title;
+        this.text = text;
+    }
+
+    public string Title { get { return title; } }
+    public string Text { get { return text; } }
 }
